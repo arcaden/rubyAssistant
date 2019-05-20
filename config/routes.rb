0a5fc748707aad73505f98ae10f2138fdc8f6c3a
Glen <glenwu2@gmail.com>
2019-05-19 23:54:44 -0400

    Remove subdomains Cant get api/webhook subdomain to work on heroku moved it into the route

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
-  
-  constraints subdomain: 'api' do
-    api_version(:module => "V1", :path => {:value => "v1"}) do
+    api_version(:module => "V1", :path => {:value => "api/v1"}) do
       resources :courses
     end
-  end
 
-  constraints subdomain: 'webhook' do
-    api_version(:module => "V1", :path => {:value => "v1"}) do
+    api_version(:module => "V1", :path => {:value => "webhook/v1"}) do
       resources :webhooks
     end
-  end
 end
  
