41b66688b289adff4229bf4fead41eeca5fc91e6
Glen <glenwu2@gmail.com>
2019-06-03 22:41:24 -0400

    Restore lost routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :welcomes
+
+  api_version(module: 'V1', path: { value: 'api/v1' }) do
+    resources :courses
+  end
+
+  api_version(module: 'V1', path: { value: 'webhook/v1' }) do
+    resources :webhooks
+  end
 end
